f6b03460002ab32f2c40e4405ca184d92c48bec1
ardee222 <ardeeche@gmail.com>
2023-04-18 21:20:59 +0800

    fixed

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,62 @@
 <script>
+  import data from "./data.json"
   let testing = false;
   let choose = false;
+  let chosen,qus,all,author,title,checked,current_ans,index,correct,worng
+  all = data.all
+  author = data.author
+  title = data.title
+  let shuffled_all = all.sort((a, b) => 0.5 - Math.random());
+  let correct_ans = []
+  let ans_list = []
+  $: index = 0;
+  $: correct = 0;
+  $: wrong = 0;
+  $:qus = shuffled_all[index][0]
+
+  $:chosen = false;
+  $:checked = false;
+
+  for(let i of shuffled_all){
+    correct_ans.push(i[1]+i[2])
+  }
+  $:current_ans = correct_ans[index]
   
+  function creat_list(){
+    let a = author.sort((a, b) => 0.5 - Math.random());
+    let b = title.sort((a, b) => 0.5 - Math.random());
+    let text = a[0]+b[0]
+    if(ans_list.length >= 3){
+      let correct_ans = shuffled_all[index][1] + shuffled_all[index][2]
+      ans_list.push(correct_ans)
+      ans_list.sort((a, b) => 0.5 - Math.random());
+      console.log(ans_list)
+      }else{
+      if(ans_list.indexOf(text === -1)){
+        ans_list.push(text)
+      }
+      $: ans_list = ans_list
+      creat_list()
+    }
+  }
+
+  function check(){
+    chosen = true;
+    if(this.innerText === current_ans){
+      correct++
+      checked = true
+    }else{
+      wrong++
+      checked = false;
+    }
+    index++
+    setTimeout(()=>{
+    ans_list = []
+    creat_list()
+    chosen = false;
+  },1000)}
+
+  window.onload = ()=>{console.log(current_ans)}
 </script>
 
 <main class="relative flex flex-col items-center m-auto max-w-md w-screen h-screen bg-[#94A7AE]">
@@ -13,14 +68,13 @@
   </div>
   <div class="flex flex-col justify-center space-y-8 mt-10">
     <div class="border rounded-lg text-center p-2  text-xl hover:bg-green-300 "
-    on:mouseup={()=>{testing = true}}>選擇題</div>
+    on:mouseup={()=>{testing = true;creat_list()}}>選擇題</div>
     <div class="border rounded-lg text-center p-2  text-xl hover:bg-green-300 "
     on:mouseup={()=>{choose = true}}>配對</div>
     <div class="border rounded-lg text-center p-2  text-xl hover:bg-green-300 "
     on:mouseup={()=>{choose = true}}>名句續寫</div>
   </div>
   <footer class="absolute right-4 bottom-4">謝卓熹個人財產</footer>
-
   {/if}
 
   {#if testing || choose}
@@ -35,19 +89,27 @@
     </div>
 
     <div class="flex space-x-4">
-      <div>對:</div>
-      <div>錯:</div>
-      <div>x/50</div>
+      <div>對:{correct}</div>
+      <div>錯:{wrong}</div>
+      <div>{index+1}/50</div>
     </div>
   </div>
 
-  <div class="border border-x-0 border-b-0 border-t-2 m-2  border-dashed max-w-sm w-screen h-10"></div>
+  <div class="border border-x-0 border-b-0 border-t-2 m-2  border-dashed max-w-sm w-screen h-10 p-2 "></div>
   {/if}
   {#if testing}
   <!--question-->
-  <div class="flex flex-col items-center">
-    <div class="border w-60 ">
-      a
+  <div class="flex flex-col items-center space-y-4 text-xl">
+    <div class="border truncate ">{qus}</div>   
+    <div class="space-y-4">
+    {#each ans_list as ans}
+      <div class="truncate  hover:bg-red-100"
+      on:mouseup={check}
+      class:border={chosen === true }
+      class:border-green-200={chosen === true && checked === true}
+      class:border-red-200={chosen === true && checked === !true}
+      >{ans}</div>
+    {/each}
     </div>    
   </div>
   {/if}
