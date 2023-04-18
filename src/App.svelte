4c675d1ff402ef71a5080a6c720fee588ea31516
ardee222 <ardeeche@gmail.com>
2023-04-18 19:55:13 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -11,23 +11,23 @@
   <div class="text-xs border border-b-0 border-x-0 max-w-sm w-screen my-2">
     有bug請見願
   </div>
-  <div class="flex flex-col justify-center space-y-5 mt-10">
+  <div class="flex flex-col justify-center space-y-8 mt-10">
     <div class="border rounded-lg text-center p-2  text-xl hover:bg-green-300 "
-    on:mouseup={()=>{testing = !testing}}>選擇題</div>
+    on:mouseup={()=>{testing = true}}>選擇題</div>
     <div class="border rounded-lg text-center p-2  text-xl hover:bg-green-300 "
-    on:mouseup={()=>{choose = !choose}}>配對</div>
+    on:mouseup={()=>{choose = true}}>配對</div>
     <div class="border rounded-lg text-center p-2  text-xl hover:bg-green-300 "
-    on:mouseup={()=>{choose = !choose}}>名句續寫</div>
+    on:mouseup={()=>{choose = true}}>名句續寫</div>
   </div>
   <footer class="absolute right-4 bottom-4">謝卓熹個人財產</footer>
 
   {/if}
 
-  {#if testing}
+  {#if testing || choose}
   <!--function_list-->
   <div class="mt-4 px-8 w-full flex items-center place-content-between ">
     <div class="hover:bg-green-300 hover:rounded-lg p-2 flex items-center text-xs text-white rounded-lg"
-    on:mouseup={()=>{testing = !testing}}>
+    on:mouseup={()=>{testing = false;choose = false}}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
         <path stroke-linecap="round" stroke-linejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
       </svg>
@@ -42,7 +42,8 @@
   </div>
 
   <div class="border border-x-0 border-b-0 border-t-2 m-2  border-dashed max-w-sm w-screen h-10"></div>
-
+  {/if}
+  {#if testing}
   <!--question-->
   <div class="flex flex-col items-center">
     <div class="border w-60 ">
